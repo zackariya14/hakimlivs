2d912a62816f60adf49b9ef260da4b8b7da2802c
Mohammed Bakhet <momo.baket@gmail.com>
2024-04-02 15:27:50 +0200

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,24 @@
+
+
+import { CopyrightOutlined, Facebook, Instagram, Twitter,  } from '@mui/icons-material';
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <div >
+    <footer className="footer-container">
+        <div className="leftFooter"> <h2>Hakims livs.</h2> <h3>Beskrivning</h3><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. A odit cupiditate error deleniti sequi est dignissimos tenetur dolore officia quo, facilis dolorem pariatur eveniet provident eos! Eos, mollitia. Id, iusto.</p> </div>
+        
+        
+        <div className="centerFooter"> <CopyrightOutlined/> 1920 Copyright: HakimLivs.com </div>
+       
+       
+       
+        <div className="rightFooter"> <Twitter/> <Instagram/> <Facebook/> </div>
+    
+    </footer>
+    </div>
+  );
+};
+
+export default Footer;
